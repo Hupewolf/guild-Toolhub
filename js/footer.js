baf6e41397c6550db0a8527427544ac2acf13db5
Thái <nnt000fit@gmail.com>
2026-02-15 01:46:26 +0700

    fix tất cả footer

---
+++ js/footer.js
@@ -0,0 +1,51 @@
+const footer = `      <div class="footer-container">
+        <div class="footer-container-top">
+            <div class="footer-container-top-left-left">
+                <div class="footer-container-top-left">
+                  <img class="footer-container-top-left-logo" src="img/trangchuimg/Logo.svg"alt=""/>
+                  <div class="footer-container-top-left-text">Tiếp cận các công cụ tiên tiến nhất trong hệ sinh thái kỹ thuật số.</div>
+                </div>
+                <div class="footer-container-top-box">
+                  <p class="footer-container-top-box-tittle">vỀ CHÚNG TÔI</p>
+                  <div class="footer-container-top-box-text">
+                      <p class="footer-container-top-box-text">Funny Guild thuộc Uside ra mắt web tổng hợp công cụ tiện ích phục vụ nhu cầu học tập và làm việc hiệu quả.</p>
+                      
+                  </div>
+                </div>
+                <div class="footer-container-top-box">
+                  <p class="footer-container-top-box-tittle">TIỆN ÍCH</p>
+                  <div class="footer-container-top-box-text">
+                      <p class="footer-container-top-box-text">Trải nghiệm hệ sinh thái công cụ số mọi lúc, mọi nơi với giao diện thân thiện và tốc độ xử lý vượt trội. </p>
+                  </div>
+                  
+                </div>
+                <div class="footer-container-top-box">
+                  <p class="footer-container-top-box-tittle">CONTACT US</p>
+                  <div class="footer-container-top-box-text">
+                      <p  style="text-transform: uppercase;">Facebook</p>
+                      <p class="none"  style="text-transform: uppercase;">tiktok</p>
+                      <p class="none"  style="text-transform: uppercase;">e-mail</p>
+                      <p class="none"  style="text-transform: uppercase;">zalo</p>
+                  </div>
+                </div>
+            </div>
+          <div class="footer-container-top-right">
+            <div class="footer-container-top-box-tittle">GỬI PHẢN HỒI</div>
+            <div class="footer-container-top-right-text">Gửi cho chúng tôi mọi phản hồi về các vấn đề bạn gặp phải.</div>
+            <div class="footer-container-top-right-input">
+                <button><img src="img/trangchuimg/icon-send.svg" alt=""></button>
+            </div>
+          </div>
+        </div>
+        <div class="footer-container-bottom">
+            <div class="footer-container-bottom-container">
+                <p class="footer-container-bottom-container-left">© 2026 TOOLHUB. ALL RIGHTS RESERVED.</p>
+                <div class="footer-container-bottom-container-right">
+                    <p style="color: #6B7280;">Phiên bản 1.0</p>
+                    <p style="color: #00F0FF;">TRẠNG THÁI: ONLINE</p>
+                </div>
+            </div>
+        </div>
+      </div>`
+
+document.getElementsByTagName("footer")[0].innerHTML = footer;
