9addacf0e9e86c0eb2a75ee3b0e5b912aa3b0d75
Hupewolf <nhannguyen.ix5@gmail.com>
2026-02-15 21:07:09 +0700

    fixbug/collection: xong search nhẹ

--- js/search.js
+++ js/search.js
@@ -1,12 +1,32 @@
+// function handleSearch(input) {
+//     input.addEventListener("keydown", (e) => {
+//         if (e.key === "Enter") {
+//             const keyword = input.value.trim();
+//             if (!keyword) return;
+//             window.location.href = `result.html?q=${encodeURIComponent(keyword)}`;
+//         }
+//     });
+// }
+// document.querySelectorAll(".head-search, .head-search2, .banner-search")
+//     .forEach(handleSearch);
+
 function handleSearch(input) {
+
+    input.addEventListener("input", (e) => {
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+    }, true);
+
     input.addEventListener("keydown", (e) => {
         if (e.key === "Enter") {
+            e.preventDefault();
             const keyword = input.value.trim();
             if (!keyword) return;
-            window.location.href = `result.html?q=${encodeURIComponent(keyword)}`;
+            window.location.href =
+                `result.html?q=${encodeURIComponent(keyword)}`;
         }
     });
 }
+
 document.querySelectorAll(".head-search, .head-search2, .banner-search")
     .forEach(handleSearch);
-
